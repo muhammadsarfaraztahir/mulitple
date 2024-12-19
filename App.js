8270b133099fa07e81711a4c3192ed7a19d55141
muhammadsarfaraztahir <sarfaraztahirarain2006@gmail.com>
2024-12-19 15:40:37 +0500

    ..

--- App.js
+++ App.js
@@ -2,17 +2,21 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import LoginPage from './screens/LoginPage.jsx'; // Adjust the path according to your project structure
-import SignupPage from './screens/SignupPage.jsx'; // Adjust the path according to your project structure
+import LoginPage from './screens/LoginPage.jsx';
+import SignupPage from './screens/SignupPage.jsx';
+import HomeScreen from './HomeScreen';
+// import DashboardPage from './DashboardPage'; // Assuming you have a DashboardPage component
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Login" component={LoginPage} />
         <Stack.Screen name="Signup" component={SignupPage} />
+        <Stack.Screen name="Dashboard" component={DashboardPage} />
       </Stack.Navigator>
     </NavigationContainer>
   );
